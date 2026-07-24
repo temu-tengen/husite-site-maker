69a026f2aaa9d5ed9a9720048fce259ee4b709a7
temu-tengen <shalin.mehta2015@gmail.com>
2026-07-24 22:23:47 +0000

    new version

--- husite/app/actions/actions.ts
+++ husite/app/actions/actions.ts
@@ -1,4 +1,5 @@
 'use server';
+import { neon } from "@neondatabase/serverless";
 
 export type FormState = {
     success: boolean;
@@ -6,5 +7,20 @@ export type FormState = {
 }
 
 export async function submitRequest(prevState: FormState, formData: FormData): Promise<FormState> {
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
+    const email = formData.get("email");
+    const requestName = formData.get("requestName");
+    const description = formData.get("description");
 
+    const sql = neon(process.env.DATABASE_URL!);
+
+    // check if user already has an email in the database
+    const usernameOfEmailIfExists = await sql `SELECT firstname FROM requests WHERE email = ${email};`;
+    if (usernameOfEmailIfExists === null) {
+        await sql `INSERT INTO requests (requestname, firstname, lastname, email, description) VALUES (${requestName}, ${firstName}, ${lastName}, ${email}, ${description});`;
+        return { success: true, message: "Your request has been sent in! We'll reply to you asap with your provided email."};
+    } else {
+        return { success: false, message: "You've already sent in an request in progress. Due to our limited staff, we can't accept more than one request per person. Try again after your first site has been completed."};
+    }
 }
--- husite/app/requestsite/page.tsx
+++ husite/app/requestsite/page.tsx
@@ -1,12 +1,28 @@
+"use client";
 import styles from "./page.module.css";
 
-import useActionState from "react";
+import { useActionState } from "react";
+import { submitRequest, FormState } from "../actions/actions";
+
+const initialState: FormState = { success: false, message: "" };
 
 export default function RequestSitePage() {
+    const [state, formAction, isPending] = useActionState(submitRequest, initialState);
     return (
         <div className={styles.page}>
             <h1>Request Your Site</h1>
 
+            <div className={styles.formContainer}>
+                <form action={formAction}>
+                    <input name="requestName" required placeholder="Site Name"/>
+                    <input name="email" placeholder="Email" required/>
+                    <input name="firstName" placeholder="First Name" required/>
+                    <input name="lastName" placeholder="Last Name" required/>
+                    <input name="description" placeholder="Description" required/>
+                    <button type="submit">{isPending ? "Sending in.." : "Submit Request"}</button>
+                    <p>{state.message}</p>
+                </form>
+            </div>
         </div>
     );
 }
